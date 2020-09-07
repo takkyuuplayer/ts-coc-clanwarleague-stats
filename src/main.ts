51dad4a9ea2ad9069615bd0621d8f0051fad16fb
takkyuuplayer <takkyuuplayer@gmail.com>
2020-09-07 18:01:01 +0900

    Refactoring

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import readline from 'readline'
 import { google, sheets_v4 } from 'googleapis'
 
 import { Coc } from './webServices/coc'
-import { receiveMessageOnPort } from 'worker_threads';
-import { content } from 'googleapis/build/src/apis/content'
-import { fork } from 'cluster'
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
@@ -83,149 +80,60 @@ async function listMajors(auth: any) {
     const client = await util.promisify(fs.readFile)('coc.json')
         .then(content => new Coc(JSON.parse(content.toString()).jwt))
     const clanTag = '#29UQ0802V'
-    const leaguegroup = await client.fetchCurrentWarLeague(clanTag)
-        .then(response => response.data)
-        .catch(console.log);
-    // const leaguegroup = await util.promisify(fs.readFile)('test/data/coc-currenwar-leaguegroup.json')
-    //     .then(content => JSON.parse(content.toString()))
-    const clan = leaguegroup.clans.find((c: any) => c.tag === clanTag)
+
+    // const leaguegroup = await client.fetchCurrentWarLeague(clanTag)
+    //     .then(response => response.data)
+    //     .catch(console.log);
+    const leaguegroup = await util.promisify(fs.readFile)('test/data/coc-currenwar-leaguegroup.json')
+        .then(content => JSON.parse(content.toString()))
 
     const sheets = google.sheets({ version: 'v4', auth });
-    // const spreadsheet = await sheets.spreadsheets.create({
-    //     requestBody: {
-    //         properties: {
-    //             title: `${clan.name} (${clan.tag}) / ${leaguegroup.season}`,
-    //         },
-    //         sheets: [
-    //             {
-    //                 properties: {
-    //                     title: "Summary",
-    //                 },
-    //             },
-    //             ...leaguegroup.clans.map((c: any) => ({ properties: { title: c.name } }))
-    //         ]
-    //     }
-    // })
-    const spreadsheet = await sheets.spreadsheets.get({
-        spreadsheetId: '1Ah-LAZLH-IS1Xazs2wYhgLxTGW94Olu7Loq53gU9ACU'
+    const spreadsheet = await sheets.spreadsheets.create({
+        requestBody: Coc.createSpreadsheetRequestBody(clanTag, leaguegroup)
     })
-    console.log(spreadsheet)
-
-    // // Update Summary Sheets
-    // await sheets.spreadsheets.values.batchUpdate({
-    //     spreadsheetId: spreadsheet.data.spreadsheetId,
-    //     requestBody: {
-    //         valueInputOption: "USER_ENTERED",
-    //         data: [
-    //             {
-    //                 range: "Summary!A1:P9",
-    //                 values: [
-    //                     [
-    //                         "Clan Name",
-    //                         "Clan Tag",
-    //                         "Members",
-    //                         ...Array.from(Array(MAX_TH_LEVEL)).map((_, idx) => `TH${MAX_TH_LEVEL - idx}`)
-    //                     ],
-    //                     ...leaguegroup.clans.map((c: any) => {
-    //                         return [
-    //                             c.name,
-    //                             c.tag,
-    //                             c.members.length,
-    //                             ...Array.from(Array(MAX_TH_LEVEL)).map((_, idx) => {
-    //                                 const len = c.members.filter((m: any) => m.townHallLevel == (MAX_TH_LEVEL - idx)).length
-    //                                 return len === 0 ? undefined : len;
-    //                             })
-    //                         ]
-    //                     })
-    //                 ]
-    //             }
-    //         ],
-    //     },
+    // const spreadsheet = await sheets.spreadsheets.get({
+    //     spreadsheetId: '1P7LOxalE1oA7Qw8GbG6oC5AcryIFXgPDMkaQOVzsC7I',
     // })
 
-    // // Initialize Each Clan Sheets
-    // const opponentsCount = leaguegroup.clans.length - 1;
-    // await sheets.spreadsheets.values.batchUpdate({
-    //     spreadsheetId: spreadsheet.data.spreadsheetId,
-    //     requestBody: {
-    //         valueInputOption: "USER_ENTERED",
-    //         data: [
-    //             ...leaguegroup.clans.map((c: any) => {
-    //                 return {
-    //                     range: `${c.name}!A1:L${c.members.length + 1}`,
-    //                     values: [
-    //                         [
-    //                             "Player Name",
-    //                             "Player Tag",
-    //                             "TH",
-    //                             "Stars",
-    //                             "Count",
-    //                             ...Array.from(Array(opponentsCount)).map((_, idx) => `R${idx + 1}`)
-    //                         ],
-    //                         ...c.members.map((m: any, idx: number) => {
-    //                             return [
-    //                                 m.name,
-    //                                 m.tag,
-    //                                 m.townHallLevel,
-    //                                 `=SUM(F${idx + 2}:L${idx + 2})`,
-    //                                 `=COUNTA(F${idx + 2}:L${idx + 2})`,
-    //                                 ...Array.from(Array(opponentsCount)).map((_, idx) => undefined)
-    //                             ]
-    //                         })
-    //                     ]
-    //                 }
-    //             })
-    //         ],
-    //     },
-    // })
+    // Initialize sheets
+    await sheets.spreadsheets.values.batchUpdate({
+        spreadsheetId: spreadsheet.data.spreadsheetId,
+        requestBody: Coc.initializeSummarySheetRequestBody(leaguegroup),
+    })
+    await sheets.spreadsheets.values.batchUpdate({
+        spreadsheetId: spreadsheet.data.spreadsheetId,
+        requestBody: Coc.initializeClanSheetRequestBody(leaguegroup),
+    })
 
-    // const members = leaguegroup.clans.reduce((prev: { [playerTag: string]: number }, c: any) => {
-    //     c.members.forEach((m: any, idx: number) => {
-    //         prev[m.tag] = idx + 2
-    //     })
-    //     return prev
-    // }, {})
-    // // console.log(members)
+    // Update War Results
+    const members = leaguegroup.clans.reduce((prev: { [playerTag: string]: number }, c: any) => {
+        c.members.forEach((m: any, idx: number) => {
+            prev[m.tag] = idx + 2
+        })
+        return prev
+    }, {})
+    let eachWarResultRequests: Array<sheets_v4.Schema$ValueRange> = []
+    for (let idx = 0; idx < leaguegroup.rounds.length; idx++) {
+        const round = leaguegroup.rounds[idx]
+        if (round.warTags[0] === '#0') {
+            break;
+        }
+        const warResults = await Promise.all(round.warTags.map((warTag: string) => {
+            return client.fetchWar(warTag).then(response => response.data);
+        }))
+        warResults.forEach((warResult: any) => {
+            eachWarResultRequests.push(...Coc.updateWarResultDataRequest(members, idx + 1, warResult));
+        })
+    }
+    await sheets.spreadsheets.values.batchUpdate({
+        spreadsheetId: spreadsheet.data.spreadsheetId,
+        requestBody: {
+            valueInputOption: "USER_ENTERED",
+            data: eachWarResultRequests,
+        }
+    })
 
-    // let eachWarResultRequests: Array<sheets_v4.Schema$ValueRange> = []
-    // for (let idx = 0; idx < leaguegroup.rounds.length; idx++) {
-    //     const round = leaguegroup.rounds[idx]
-    //     if (round.warTags[0] === '#0') {
-    //         break;
-    //     }
-
-    //     const warResults = await Promise.all(round.warTags.map((warTag: string) => {
-    //         return client.fetchWar(warTag).then(response => response.data);
-    //     }))
-    //     const column = String.fromCharCode('F'.charCodeAt(0) + idx)
-    //     warResults.forEach((warResult: any) => {
-    //         eachWarResultRequests.push(...warResult.clan.members.map((m: any) => {
-    //             const cell = `${column}${members[m.tag]}`
-    //             const range = `${warResult.clan.name}!${cell}:${cell}`
-    //             return {
-    //                 range,
-    //                 values: [[m.attacks ? m.attacks[0].stars : "?"]]
-    //             }
-    //         }))
-    //         eachWarResultRequests.push(...warResult.opponent.members.map((m: any) => {
-    //             const cell = `${column}${members[m.tag]}`
-    //             const range = `${warResult.opponent.name}!${cell}:${cell}`
-    //             return {
-    //                 range,
-    //                 values: [[m.attacks ? m.attacks[0].stars : "?"]]
-    //             }
-    //         }))
-    //     })
-    // }
-    // await sheets.spreadsheets.values.batchUpdate({
-    //     spreadsheetId: spreadsheet.data.spreadsheetId,
-    //     requestBody: {
-    //         valueInputOption: "USER_ENTERED",
-    //         data: eachWarResultRequests,
-    //     }
-    // })
-
-    // Freeze, Resize sheets.
+    // Resize Columns
     await sheets.spreadsheets.batchUpdate({
         spreadsheetId: spreadsheet.data.spreadsheetId,
         requestBody: {
@@ -240,46 +148,11 @@ async function listMajors(auth: any) {
                         }
                     }
                 })),
-                ...spreadsheet.data.sheets!.map((sheet) => ({
-                    updateSheetProperties: {
-                        properties: {
-                            sheetId: sheet.properties!.sheetId,
-                            gridProperties: {
-                                frozenRowCount: 1,
-                            }
-                        },
-                        fields: 'gridProperties.frozenRowCount'
-                    }
-                })),
-                ...spreadsheet.data.sheets!
-                    .filter(sheet => sheet.properties?.title !== 'Summary')
-                    .map((sheet) => ({
-                        updateSheetProperties: {
-                            properties: {
-                                sheetId: sheet.properties!.sheetId,
-                                gridProperties: {
-                                    frozenColumnCount: 5,
-                                },
-                            },
-                            fields: 'gridProperties.frozenColumnCount'
-                        }
-                    })),
-                ...spreadsheet.data.sheets!
-                    .filter(sheet => sheet.properties?.title === 'Summary')
-                    .map((sheet) => ({
-                        updateSheetProperties: {
-                            properties: {
-                                sheetId: sheet.properties!.sheetId,
-                                gridProperties: {
-                                    frozenColumnCount: 3,
-                                },
-                            },
-                            fields: 'gridProperties.frozenColumnCount'
-                        }
-                    })),
             ]
         }
     })
+
+    console.log(spreadsheet.data.spreadsheetUrl)
 
     // Add Each Clan Sheets
     // const addSheetRequests: Array<sheets_v4.Schema$Request> = leaguegroup.clans.filter((c: any) => {

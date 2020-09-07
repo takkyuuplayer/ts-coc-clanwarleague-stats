88198b2b6129f33d5e08201d2565f8615506c8b4
takkyuuplayer <takkyuuplayer@gmail.com>
2020-09-07 21:16:23 +0900

    Lint

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
-import fs from 'fs'
-import util from 'util'
-import readline from 'readline'
-import { google, sheets_v4 } from 'googleapis'
+import fs from "fs";
+import util from "util";
+import readline from "readline";
+import { google, sheets_v4 } from "googleapis";
 
-import { Coc } from './webServices/coc'
+import { Coc } from "./webServices/coc";
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'token.json';
+const TOKEN_PATH = "token.json";
 
 // Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    // Authorize a client with credentials, then call the Google Sheets API.
-    authorize(JSON.parse(content.toString()), listMajors);
+fs.readFile("credentials.json", (err, content) => {
+  if (err) return console.log("Error loading client secret file:", err);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  authorize(JSON.parse(content.toString()), listMajors);
 });
 
 /**
@@ -26,16 +26,19 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials: any, callback: typeof listMajors) {
-    const { client_secret, client_id, redirect_uris } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
 
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client, callback);
-        oAuth2Client.setCredentials(JSON.parse(token.toString()));
-        callback(oAuth2Client);
-    });
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token.toString()));
+    callback(oAuth2Client);
+  });
 }
 
 /**
@@ -45,28 +48,32 @@ function authorize(credentials: any, callback: typeof listMajors) {
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
 function getNewToken(oAuth2Client: any, callback: typeof listMajors) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+  console.log("Authorize this app by visiting this url:", authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question("Enter the code from that page here: ", (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err: any, token: any) => {
+      if (err)
+        return console.error(
+          "Error while trying to retrieve access token",
+          err
+        );
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log("Token stored to", TOKEN_PATH);
+      });
+      callback(oAuth2Client);
     });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err: any, token: any) => {
-            if (err) return console.error('Error while trying to retrieve access token', err);
-            oAuth2Client.setCredentials(token);
-            // Store the token to disk for later program executions
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) return console.error(err);
-                console.log('Token stored to', TOKEN_PATH);
-            });
-            callback(oAuth2Client);
-        });
-    });
+  });
 }
 
 /**
@@ -75,67 +82,76 @@ function getNewToken(oAuth2Client: any, callback: typeof listMajors) {
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
 async function listMajors(auth: any) {
-    const client = await util.promisify(fs.readFile)('coc.json')
-        .then(content => new Coc(JSON.parse(content.toString()).jwt))
-    const clanTag = '#29UQ0802V'
+  const client = await util
+    .promisify(fs.readFile)("coc.json")
+    .then((content) => new Coc(JSON.parse(content.toString()).jwt));
+  const clanTag = "#29UQ0802V";
 
-    const leaguegroup = await client.fetchCurrentWarLeague(clanTag)
-        .then(response => response.data)
-        .catch(console.log);
-    // const leaguegroup = await util.promisify(fs.readFile)('test/data/coc-currenwar-leaguegroup.json')
-    //     .then(content => JSON.parse(content.toString()))
+  const leaguegroup = await client
+    .fetchCurrentWarLeague(clanTag)
+    .then((response) => response.data)
+    .catch(console.log);
+  // const leaguegroup = await util.promisify(fs.readFile)('test/data/coc-currenwar-leaguegroup.json')
+  //     .then(content => JSON.parse(content.toString()))
 
-    const sheets = google.sheets({ version: 'v4', auth });
-    const spreadsheet = await sheets.spreadsheets.create({
-        requestBody: Coc.createSpreadsheetRequestBody(clanTag, leaguegroup)
-    })
-    // const spreadsheet = await sheets.spreadsheets.get({
-    //     spreadsheetId: '1P7LOxalE1oA7Qw8GbG6oC5AcryIFXgPDMkaQOVzsC7I',
-    // })
+  const sheets = google.sheets({ version: "v4", auth });
+  const spreadsheet = await sheets.spreadsheets.create({
+    requestBody: Coc.createSpreadsheetRequestBody(clanTag, leaguegroup),
+  });
+  // const spreadsheet = await sheets.spreadsheets.get({
+  //     spreadsheetId: '1P7LOxalE1oA7Qw8GbG6oC5AcryIFXgPDMkaQOVzsC7I',
+  // })
 
-    // Initialize sheets
-    await sheets.spreadsheets.values.batchUpdate({
-        spreadsheetId: spreadsheet.data.spreadsheetId,
-        requestBody: Coc.initializeSummarySheetRequestBody(leaguegroup),
-    })
-    await sheets.spreadsheets.values.batchUpdate({
-        spreadsheetId: spreadsheet.data.spreadsheetId,
-        requestBody: Coc.initializeClanSheetRequestBody(leaguegroup),
-    })
+  // Initialize sheets
+  await sheets.spreadsheets.values.batchUpdate({
+    spreadsheetId: spreadsheet.data.spreadsheetId,
+    requestBody: Coc.initializeSummarySheetRequestBody(leaguegroup),
+  });
+  await sheets.spreadsheets.values.batchUpdate({
+    spreadsheetId: spreadsheet.data.spreadsheetId,
+    requestBody: Coc.initializeClanSheetRequestBody(leaguegroup),
+  });
 
-    // Update War Results
-    const members = leaguegroup.clans.reduce((prev: { [playerTag: string]: number }, c: any) => {
-        c.members.forEach((m: any, idx: number) => {
-            prev[m.tag] = idx + 2
-        })
-        return prev
-    }, {})
-    let eachWarResultRequests: Array<sheets_v4.Schema$ValueRange> = []
-    for (let idx = 0; idx < leaguegroup.rounds.length; idx++) {
-        const round = leaguegroup.rounds[idx]
-        if (round.warTags[0] === '#0') {
-            break;
-        }
-        const warResults = await Promise.all(round.warTags.map((warTag: string) => {
-            return client.fetchWar(warTag).then(response => response.data);
-        }))
-        warResults.forEach((warResult: any) => {
-            eachWarResultRequests.push(...Coc.updateWarResultDataRequest(members, idx + 1, warResult));
-        })
+  // Update War Results
+  const members = leaguegroup.clans.reduce(
+    (prev: { [playerTag: string]: number }, c: any) => {
+      c.members.forEach((m: any, idx: number) => {
+        prev[m.tag] = idx + 2;
+      });
+      return prev;
+    },
+    {}
+  );
+  const eachWarResultRequests: Array<sheets_v4.Schema$ValueRange> = [];
+  for (let idx = 0; idx < leaguegroup.rounds.length; idx++) {
+    const round = leaguegroup.rounds[idx];
+    if (round.warTags[0] === "#0") {
+      break;
     }
-    await sheets.spreadsheets.values.batchUpdate({
-        spreadsheetId: spreadsheet.data.spreadsheetId,
-        requestBody: {
-            valueInputOption: "USER_ENTERED",
-            data: eachWarResultRequests,
-        }
-    })
+    const warResults = await Promise.all(
+      round.warTags.map((warTag: string) => {
+        return client.fetchWar(warTag).then((response) => response.data);
+      })
+    );
+    warResults.forEach((warResult: any) => {
+      eachWarResultRequests.push(
+        ...Coc.updateWarResultDataRequest(members, idx + 1, warResult)
+      );
+    });
+  }
+  await sheets.spreadsheets.values.batchUpdate({
+    spreadsheetId: spreadsheet.data.spreadsheetId,
+    requestBody: {
+      valueInputOption: "USER_ENTERED",
+      data: eachWarResultRequests,
+    },
+  });
 
-    // Resize Columns
-    await sheets.spreadsheets.batchUpdate({
-        spreadsheetId: spreadsheet.data.spreadsheetId,
-        requestBody: Coc.resizeColumnRequestBody(spreadsheet),
-    })
+  // Resize Columns
+  await sheets.spreadsheets.batchUpdate({
+    spreadsheetId: spreadsheet.data.spreadsheetId,
+    requestBody: Coc.resizeColumnRequestBody(spreadsheet),
+  });
 
-    console.log(spreadsheet.data.spreadsheetUrl)
+  console.log(spreadsheet.data.spreadsheetUrl);
 }
--- src/webServices/coc.ts
+++ src/webServices/coc.ts
@@ -1,182 +1,191 @@
-import axios, { AxiosInstance, AxiosPromise } from 'axios'
-import qs from 'querystring'
-import { sheets_v4 } from 'googleapis'
-import { GaxiosResponse } from 'gaxios';
+import axios, { AxiosInstance, AxiosPromise } from "axios";
+import qs from "querystring";
+import { sheets_v4 } from "googleapis";
+import { GaxiosResponse } from "gaxios";
 
 const MAX_TH_LEVEL = 13;
 
-const SUMMARY_SHEET_NAME = 'Summary'
-const SUMMARY_SHEET_HEADER = [
-    "Clan Name",
-    "Clan Tag",
-    "Members",
-]
+const SUMMARY_SHEET_NAME = "Summary";
+const SUMMARY_SHEET_HEADER = ["Clan Name", "Clan Tag", "Members"];
 
-const CLAN_SHEET_HEADER = [
-    "Player Name",
-    "Player Tag",
-    "TH",
-    "Stars",
-    "Count",
-]
+const CLAN_SHEET_HEADER = ["Player Name", "Player Tag", "TH", "Stars", "Count"];
 
 export class Coc {
-    private client: AxiosInstance
+  private client: AxiosInstance;
 
-    constructor(jwt: string) {
-        this.client = axios.create({
-            baseURL: `https://api.clashofclans.com/v1`,
-            headers: {
-                'Authorization': `Bearer ${jwt}`
-            }
-        })
-    }
+  constructor(jwt: string) {
+    this.client = axios.create({
+      baseURL: `https://api.clashofclans.com/v1`,
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    });
+  }
 
-    public fetchCurrentWarLeague(clanTag: string): AxiosPromise {
-        const url = `clans/${qs.escape(clanTag)}/currentwar/leaguegroup`
-        return this.client.get(url);
-    }
+  public fetchCurrentWarLeague(clanTag: string): AxiosPromise {
+    const url = `clans/${qs.escape(clanTag)}/currentwar/leaguegroup`;
+    return this.client.get(url);
+  }
 
-    public fetchWar(warTag: string): AxiosPromise {
-        return this.client.get(`clanwarleagues/wars/${qs.escape(warTag)}`);
-    }
+  public fetchWar(warTag: string): AxiosPromise {
+    return this.client.get(`clanwarleagues/wars/${qs.escape(warTag)}`);
+  }
 
-    public static createSpreadsheetRequestBody(myClanTag: string, leaguegroup: any): sheets_v4.Schema$Spreadsheet {
-        const myClan = leaguegroup.clans.find((clan: any) => clan.tag === myClanTag)
-        return {
-            properties: {
-                title: `${myClan.name} (${myClan.tag}) / ${leaguegroup.season}`,
+  public static createSpreadsheetRequestBody(
+    myClanTag: string,
+    leaguegroup: any
+  ): sheets_v4.Schema$Spreadsheet {
+    const myClan = leaguegroup.clans.find(
+      (clan: any) => clan.tag === myClanTag
+    );
+    return {
+      properties: {
+        title: `${myClan.name} (${myClan.tag}) / ${leaguegroup.season}`,
+      },
+      sheets: [
+        {
+          properties: {
+            title: SUMMARY_SHEET_NAME,
+            gridProperties: {
+              frozenRowCount: 1,
+              frozenColumnCount: SUMMARY_SHEET_HEADER.length,
             },
-            sheets: [
-                {
-                    properties: {
-                        title: SUMMARY_SHEET_NAME,
-                        gridProperties: {
-                            frozenRowCount: 1,
-                            frozenColumnCount: SUMMARY_SHEET_HEADER.length,
-                        }
-                    },
-                },
-                ...leaguegroup.clans.map((clan: any) => ({
-                    properties: {
-                        title: clan.name,
-                        gridProperties: {
-                            frozenRowCount: 1,
-                            frozenColumnCount: CLAN_SHEET_HEADER.length,
-                        }
-                    }
-                }))
-            ]
-        }
-    }
+          },
+        },
+        ...leaguegroup.clans.map((clan: any) => ({
+          properties: {
+            title: clan.name,
+            gridProperties: {
+              frozenRowCount: 1,
+              frozenColumnCount: CLAN_SHEET_HEADER.length,
+            },
+          },
+        })),
+      ],
+    };
+  }
 
-    public static initializeSummarySheetRequestBody(leaguegroup: any): sheets_v4.Schema$BatchUpdateValuesRequest {
-        const column = String.fromCharCode('A'.charCodeAt(0) + MAX_TH_LEVEL + SUMMARY_SHEET_HEADER.length - 1)
-        const townhallLevels = Array.from(Array(MAX_TH_LEVEL)).map((_, idx) => (MAX_TH_LEVEL - idx));
-        return {
-            valueInputOption: "USER_ENTERED",
-            data: [
-                {
-                    range: `${SUMMARY_SHEET_NAME}!A1:${column}${leaguegroup.clans.length + 1}`,
-                    values: [
-                        [
-                            ...SUMMARY_SHEET_HEADER,
-                            ...townhallLevels.map((level) => `TH${level}`)
-                        ],
-                        ...leaguegroup.clans.map((clan: any) => {
-                            return [
-                                clan.name,
-                                clan.tag,
-                                clan.members.length,
-                                ...townhallLevels.map((level) => {
-                                    const len = clan.members.filter((member: any) => member.townHallLevel == level).length
-                                    return len === 0 ? undefined : len;
-
-                                })
-                            ]
-                        })
-                    ]
-                }
+  public static initializeSummarySheetRequestBody(
+    leaguegroup: any
+  ): sheets_v4.Schema$BatchUpdateValuesRequest {
+    const column = String.fromCharCode(
+      "A".charCodeAt(0) + MAX_TH_LEVEL + SUMMARY_SHEET_HEADER.length - 1
+    );
+    const townhallLevels = Array.from(Array(MAX_TH_LEVEL)).map(
+      (_, idx) => MAX_TH_LEVEL - idx
+    );
+    return {
+      valueInputOption: "USER_ENTERED",
+      data: [
+        {
+          range: `${SUMMARY_SHEET_NAME}!A1:${column}${
+            leaguegroup.clans.length + 1
+          }`,
+          values: [
+            [
+              ...SUMMARY_SHEET_HEADER,
+              ...townhallLevels.map((level) => `TH${level}`),
             ],
-        };
-    }
+            ...leaguegroup.clans.map((clan: any) => {
+              return [
+                clan.name,
+                clan.tag,
+                clan.members.length,
+                ...townhallLevels.map((level) => {
+                  const len = clan.members.filter(
+                    (member: any) => member.townHallLevel == level
+                  ).length;
+                  return len === 0 ? undefined : len;
+                }),
+              ];
+            }),
+          ],
+        },
+      ],
+    };
+  }
 
-    public static initializeClanSheetRequestBody(leaguegroup: any): sheets_v4.Schema$BatchUpdateValuesRequest {
-
-        const opponentsCount = leaguegroup.clans.length - 1;
-        const rounds = Array.from(Array(opponentsCount)).map((_, idx) => `R${idx + 1}`);
-        const columnStart = String.fromCharCode('A'.charCodeAt(0) + CLAN_SHEET_HEADER.length)
-        const columnEnd = String.fromCharCode(columnStart.charCodeAt(0) + opponentsCount - 1);
-        return {
-            valueInputOption: "USER_ENTERED",
-            data: [
-                ...leaguegroup.clans.map((clan: any) => {
-                    return {
-                        range: `${clan.name}!A1:${columnEnd}${clan.members.length + 1}`,
-                        values: [
-                            [
-                                ...CLAN_SHEET_HEADER,
-                                ...rounds,
-                            ],
-                            ...clan.members.map((m: any, idx: number) => {
-                                const row = idx + 2;
-                                return [
-                                    m.name,
-                                    m.tag,
-                                    m.townHallLevel,
-                                    `=SUM(${columnStart}${row}:${columnEnd}${row})`,
-                                    `=COUNTA(${columnStart}${row}:${columnEnd}${row})`,
-                                    ...rounds.map(() => undefined),
-                                ]
-                            })
-                        ]
-                    }
-                })
+  public static initializeClanSheetRequestBody(
+    leaguegroup: any
+  ): sheets_v4.Schema$BatchUpdateValuesRequest {
+    const opponentsCount = leaguegroup.clans.length - 1;
+    const rounds = Array.from(Array(opponentsCount)).map(
+      (_, idx) => `R${idx + 1}`
+    );
+    const columnStart = String.fromCharCode(
+      "A".charCodeAt(0) + CLAN_SHEET_HEADER.length
+    );
+    const columnEnd = String.fromCharCode(
+      columnStart.charCodeAt(0) + opponentsCount - 1
+    );
+    return {
+      valueInputOption: "USER_ENTERED",
+      data: [
+        ...leaguegroup.clans.map((clan: any) => {
+          return {
+            range: `${clan.name}!A1:${columnEnd}${clan.members.length + 1}`,
+            values: [
+              [...CLAN_SHEET_HEADER, ...rounds],
+              ...clan.members.map((m: any, idx: number) => {
+                const row = idx + 2;
+                return [
+                  m.name,
+                  m.tag,
+                  m.townHallLevel,
+                  `=SUM(${columnStart}${row}:${columnEnd}${row})`,
+                  `=COUNTA(${columnStart}${row}:${columnEnd}${row})`,
+                  ...rounds.map(() => undefined),
+                ];
+              }),
             ],
-        };
-    }
+          };
+        }),
+      ],
+    };
+  }
 
-    public static updateWarResultDataRequest(
-        members: { [playerTag: string]: number },
-        round: number,
-        warResult: any
-    ): Array<sheets_v4.Schema$ValueRange> {
-        const column = String.fromCharCode('F'.charCodeAt(0) + round - 1)
-        return [
-            ...warResult.clan.members.map((m: any) => {
-                const cell = `${column}${members[m.tag]}`
-                const range = `${warResult.clan.name}!${cell}:${cell}`
-                return {
-                    range,
-                    values: [[m.attacks ? m.attacks[0].stars : "?"]]
-                }
-            }),
-            ...warResult.opponent.members.map((m: any) => {
-                const cell = `${column}${members[m.tag]}`
-                const range = `${warResult.opponent.name}!${cell}:${cell}`
-                return {
-                    range,
-                    values: [[m.attacks ? m.attacks[0].stars : "?"]]
-                }
-            }),
-        ];
-    }
-
-    public static resizeColumnRequestBody(spreadsheet: GaxiosResponse<sheets_v4.Schema$Spreadsheet>) {
+  public static updateWarResultDataRequest(
+    members: { [playerTag: string]: number },
+    round: number,
+    warResult: any
+  ): Array<sheets_v4.Schema$ValueRange> {
+    const column = String.fromCharCode("F".charCodeAt(0) + round - 1);
+    return [
+      ...warResult.clan.members.map((m: any) => {
+        const cell = `${column}${members[m.tag]}`;
+        const range = `${warResult.clan.name}!${cell}:${cell}`;
         return {
-            requests: [
-                ...spreadsheet.data.sheets!.map((sheet) => ({
-                    autoResizeDimensions: {
-                        dimensions: {
-                            sheetId: sheet.properties!.sheetId,
-                            dimension: "COLUMNS",
-                            startIndex: 0,
-                            endIndex: 26,
-                        }
-                    }
-                })),
-            ]
-        }
+          range,
+          values: [[m.attacks ? m.attacks[0].stars : "?"]],
+        };
+      }),
+      ...warResult.opponent.members.map((m: any) => {
+        const cell = `${column}${members[m.tag]}`;
+        const range = `${warResult.opponent.name}!${cell}:${cell}`;
+        return {
+          range,
+          values: [[m.attacks ? m.attacks[0].stars : "?"]],
+        };
+      }),
+    ];
+  }
 
-    }
+  public static resizeColumnRequestBody(
+    spreadsheet: GaxiosResponse<sheets_v4.Schema$Spreadsheet>
+  ) {
+    return {
+      requests: [
+        ...spreadsheet.data.sheets!.map((sheet) => ({
+          autoResizeDimensions: {
+            dimensions: {
+              sheetId: sheet.properties!.sheetId,
+              dimension: "COLUMNS",
+              startIndex: 0,
+              endIndex: 26,
+            },
+          },
+        })),
+      ],
+    };
+  }
 }
